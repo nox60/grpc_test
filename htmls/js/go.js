00b194852424c483ba3022c6e67a65ca6a3c31ad
Nox.Liu <nox60@hotmail.com>
2021-05-08 23:42:09 +0800

    add files

--- htmls/js/go.js
+++ htmls/js/go.js
@@ -1,21 +1,23 @@
-$('.first_btn').click(function () {
+$(function (){
+    $('.first_btn').click(function () {
 
-    var a = $("input[name='selectedfile']:checked").val();
-    alert("选中的radio的值是：",a);
+        var a = $("input[name='selectedfile']:checked")[0].defaultValue;
+        console.log("选中的radio的值是："+a);
 
-    $('#words').val()
-    // var words = $('#words').val()
-    // $.ajax({
-    //     url:'/api/send/'+words,
-    //     type:'GET',
-    //     dataType:'json',
-    //     contentType:"application/json",
-    //     success:function(data){
-    //         if( data.result == -990) {
-    //             alert('error！');
-    //         } else {
-    //             alert(data.msg);
-    //         }
-    //     }
-    // });
+        $('#words').val()
+        // var words = $('#words').val()
+        // $.ajax({
+        //     url:'/api/send/'+words,
+        //     type:'GET',
+        //     dataType:'json',
+        //     contentType:"application/json",
+        //     success:function(data){
+        //         if( data.result == -990) {
+        //             alert('error！');
+        //         } else {
+        //             alert(data.msg);
+        //         }
+        //     }
+        // });
+    })
 })
