92fe6076667435e3ad1357eda9a6d790c6a9b98e
LiuLi <nox60@hotmail.com>
2021-05-08 12:26:24 +0800

    Update files

--- htmls/js/go.js
+++ htmls/js/go.js
@@ -1,16 +1,21 @@
 $('.first_btn').click(function () {
-    var words = $('#words').val()
-    $.ajax({
-        url:'/api/send/'+words,
-        type:'GET',
-        dataType:'json',
-        contentType:"application/json",
-        success:function(data){
-            if( data.result == -990) {
-                alert('error！');
-            } else {
-                alert(data.msg);
-            }
-        }
-    });
+
+    var a = $("input[name='selectedfile']:checked").val();
+    alert("选中的radio的值是：",a);
+
+    $('#words').val()
+    // var words = $('#words').val()
+    // $.ajax({
+    //     url:'/api/send/'+words,
+    //     type:'GET',
+    //     dataType:'json',
+    //     contentType:"application/json",
+    //     success:function(data){
+    //         if( data.result == -990) {
+    //             alert('error！');
+    //         } else {
+    //             alert(data.msg);
+    //         }
+    //     }
+    // });
 })
