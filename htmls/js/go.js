0a6b30bd07b8e5bf27a37efa54e913c5d247b010
Nox.Liu <nox60@hotmail.com>
2021-05-09 00:13:27 +0800

    add files

--- htmls/js/go.js
+++ htmls/js/go.js
@@ -1,23 +1,30 @@
-$(function (){
+$(function () {
     $('.first_btn').click(function () {
 
         var a = $("input[name='selectedfile']:checked")[0].defaultValue;
-        console.log("选中的radio的值是："+a);
+        console.log("选中的radio的值是：" + a);
 
-        $('#words').val()
-        // var words = $('#words').val()
-        // $.ajax({
-        //     url:'/api/send/'+words,
-        //     type:'GET',
-        //     dataType:'json',
-        //     contentType:"application/json",
-        //     success:function(data){
-        //         if( data.result == -990) {
-        //             alert('error！');
-        //         } else {
-        //             alert(data.msg);
-        //         }
-        //     }
-        // });
+        if (a === 'NEWFILE') {
+            a = $('#newfilename').val() + '.txt'
+            console.log("选择新文件，文件名：", a)
+        }
+
+        console.log("输入的文件内容：", $('#words').val())
+
+        var sendValue = a + "|!!..++|" + $('#words').val();
+
+        $.ajax({
+            url:'/api/send/'+sendValue,
+            type:'GET',
+            dataType:'json',
+            contentType:"application/json",
+            success:function(data){
+                if( data.result == -990) {
+                    alert('error！');
+                } else {
+                    alert(data.msg);
+                }
+            }
+        });
     })
 })
