7151290d51e2d30cfdf8e5ce32ae7898bd6f2354
LiuLi <nox60@hotmail.com>
2021-05-25 11:50:22 +0800

    Update files

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
+	"io/ioutil"
 	"log"
 	pb "message/message"
 	"net/http"
@@ -30,6 +32,29 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Main website",
 		})
+	})
+
+	api.POST("/uploadfile", func(c *gin.Context) {
+		//获取表单数据 参数为name值
+		f, err := c.FormFile("f1")
+		//错误处理
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err,
+			})
+			return
+		} else {
+			file, _ := f.Open()
+			fileConent, _ := ioutil.ReadAll(file)
+			fmt.Println(fileConent)
+			////将文件保存至本项目根目录中
+			//c.SaveUploadedFile(f, f.Filename)
+			//保存成功返回正确的Json数据
+			c.JSON(http.StatusOK, gin.H{
+				"message": "OK",
+			})
+		}
+
 	})
 
 	api.GET("/send/:word", func(c1 *gin.Context) {
